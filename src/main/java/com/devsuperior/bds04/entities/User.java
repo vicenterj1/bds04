0c6da26e3742ff2c6ba2ef7d003355bd3c98f48e
vicenterj1 <vicenterj1@gmail.com>
2023-07-30 16:47:18 -0300

    Ajustes dos textos de validações

---
+++ src/main/java/com/devsuperior/bds04/entities/User.java
@@ -0,0 +1,133 @@
+package com.devsuperior.bds04.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements UserDetails, Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	private String email;
+	private String password;
+	
+// força que sempre venha a lista de roles do usuário	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name ="role_id"))
+	private Set<Role> roles = new HashSet<>();
+	
+	public User() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// converter o tipo role para Authority
+		
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		// porque o username é o email
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// não estamos preocupados por enquanto
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// não estamos preocupados por enquanto
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// não estamos preocupados por enquanto
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// não estamos preocupados por enquanto
+		return true;
+	}
+	
+	
+}
